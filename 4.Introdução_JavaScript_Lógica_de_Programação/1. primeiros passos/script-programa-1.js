1bc1b9590ac33aff0c4ad12628b70107c35f9f80
Thais Camargo <thaisp.camargo@gmail.com>
2022-04-06 18:12:27 -0300

    Resolve exercício 2

--- 4.Introdução_JavaScript_Lógica_de_Programação/1. primeiros passos/script-programa-1.js
+++ 4.Introdução_JavaScript_Lógica_de_Programação/1. primeiros passos/script-programa-1.js
@@ -17,3 +17,16 @@ console.log(divisao);
 
 const modulo = a % b;
 console.log(modulo);
+
+//Exercício 2:
+
+const primeiroNum = 25;
+const segundoNum = 35;
+
+if (primeiroNum > segundoNum) {
+  console.log("O primeiro número é maior: " + primeiroNum);
+} else if (primeiroNum == segundoNum) {
+  console.log("Os números são iguais");
+} else {
+  console.log("O segundo número é maior: " + segundoNum);
+}
