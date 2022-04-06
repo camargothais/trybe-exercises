6bffbcf15eb5fd55d97a0b116e9e99f37e86cdc1
Thais Camargo <thaisp.camargo@gmail.com>
2022-04-06 19:45:48 -0300

    Resolve exercício 10

--- 4.Introdução_JavaScript_Lógica_de_Programação/1. primeiros passos/script-programa-1.js
+++ 4.Introdução_JavaScript_Lógica_de_Programação/1. primeiros passos/script-programa-1.js
@@ -160,3 +160,15 @@ if (
 } else {
   console.log("false");
 }
+
+//Exercício 10:
+
+const valorDeCusto = 25;
+const valorDeVenda = 50;
+const valorRealCusto = valorDeCusto * 1.2;
+const lucro = (valorDeVenda - valorRealCusto) * 1000;
+if (valorDeCusto > 0 && valorDeVenda > 0) {
+  console.log("O valor do lucro é: " + lucro);
+} else {
+  console.log("Insira um valor maior que 0.");
+}
