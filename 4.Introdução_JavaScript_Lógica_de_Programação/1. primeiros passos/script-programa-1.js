fc883c42f37a02597f5ee854b69bb0bc1ab3636b
Thais Camargo <thaisp.camargo@gmail.com>
2022-04-06 18:12:53 -0300

    Resolve exercício 3

--- 4.Introdução_JavaScript_Lógica_de_Programação/1. primeiros passos/script-programa-1.js
+++ 4.Introdução_JavaScript_Lógica_de_Programação/1. primeiros passos/script-programa-1.js
@@ -30,3 +30,19 @@ if (primeiroNum > segundoNum) {
 } else {
   console.log("O segundo número é maior: " + segundoNum);
 }
+
+//Exercício 3:
+
+const num1 = 95;
+const num2 = 105;
+const num3 = 35;
+
+if (num1 > num2 && num1 > num3) {
+  console.log("O número 1 é o maior entre os 3 números: " + num1);
+} else if (num2 > num1 && num2 > num3) {
+  console.log("O número 2 é o maior entre os 3 números: " + num2);
+} else if (num3 > num1 && num3 > num2) {
+  console.log("O número 3 é o maior entre os 3 números: " + num3);
+} else {
+  console.log("Defina números que não são iguais.");
+}
