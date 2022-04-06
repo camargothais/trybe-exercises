0f27cac65e79b46f074998ed5db45b58cd4d3222
Thais Camargo <thaisp.camargo@gmail.com>
2022-04-06 18:30:58 -0300

    Resolve exercício 5

--- 4.Introdução_JavaScript_Lógica_de_Programação/1. primeiros passos/script-programa-1.js
+++ 4.Introdução_JavaScript_Lógica_de_Programação/1. primeiros passos/script-programa-1.js
@@ -58,3 +58,18 @@ if (valor > 0) {
 } else {
   console.log("zero");
 }
+
+//Exercício 5:
+
+const angulo1 = 45;
+const angulo2 = 45;
+const angulo3 = 90;
+
+const total = angulo1 + angulo2 + angulo3;
+if (total == 180) {
+  console.log("true");
+} else if (total !== 180 && total >= 0) {
+  console.log("false");
+} else if (angulo1 < 0 || angulo2 < 0 || angulo3 < 0) {
+  console.log("Erro. Os angulos devem ser maiores que 0.");
+}
